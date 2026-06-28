4db6f8788e9ffb4ccf4d20cd8cbe37cbdddef85c
Dhiraj <sauravdhiraj83@gmail.com>
2026-06-29 00:48:37 +0530

    authentication of user from database

--- api/UserRoute.js
+++ api/UserRoute.js
@@ -1,10 +1,12 @@
 import express, { response, Router } from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import userSchema from "../schema/User.js";
+import mongoose from "mongoose";
 
 const router = express.Router();
 
-const users = [];
+const users = mongoose.model("User", userSchema);
 
 async function hash(password) {
   const saltRounds = Number(process.env.SALT_ROUNDS);
@@ -19,7 +21,7 @@ router.post("/login", async (req, res) => {
       return res.sendStatus(400);
     }
 
-    const userFound = users.find((user) => user.email === userData.email);
+    const userFound = await users.findOne({ email: userData.email }).exec();
     if (!userFound) {
       return res.status(401).send({ message: "User not Found." });
     }
@@ -28,7 +30,10 @@ router.post("/login", async (req, res) => {
       return res.status(401).send({ message: "Password incorrect." });
     }
 
-    const token = jwt.sign({ email: userData.email }, process.env.PRIVATE_KEY);
+    const token = jwt.sign(
+      { email: userData.email, id: userFound.id },
+      process.env.PRIVATE_KEY,
+    );
     res.send({ token: token });
   } catch (error) {
     res.status(500).send({ error: error });
@@ -43,12 +48,16 @@ router.post("/register", async (req, res) => {
       return res.sendStatus(400);
     }
 
-    users.push({
+    const newUser = await users.insertOne({
       email: userData.email,
       password: await hash(userData.password),
+      name: userData.name,
     });
 
-    const token = jwt.sign({ email: userData.email }, process.env.PRIVATE_KEY);
+    const token = jwt.sign(
+      { email: userData.email, id: newUser.id },
+      process.env.PRIVATE_KEY,
+    );
     res.send({ token: token });
   } catch (error) {
     res.status(500).send({ error: error.toString() });
