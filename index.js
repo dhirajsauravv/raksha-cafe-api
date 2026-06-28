e28f5f67176304ecf0e6c873c79474088cf391b6
Dhiraj <sauravdhiraj83@gmail.com>
2026-06-28 14:23:19 +0530

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import "dotenv/config";
 import api from "./api/Api.js";
+import cors from "cors";
 
 const app = express();
+
+if (process.env.NODE_ENV === "dev") app.use(cors());
 
 const port = process.env.PORT || 5000;
 
